dfea1fc7aec23f89301c8d2894d2bd0d2f33ed75
Deepanshi Singh <69765597+Deep062001@users.noreply.github.com>
2023-03-15 02:13:17 +0530

    Update index.js

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ app.use(cors());
 app.use('/foodItems', foodItemRoutes);
 app.use('/offers', offerRoutes);
 
-const CONNECTION_URL = "mongodb+srv://deepanshi10j:alohomora@cluster0.kalnu.mongodb.net/?retryWrites=true&w=majority";
+const CONNECTION_URL = "URL";
 const PORT = process.env.PORT || 5000;
 
 mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
